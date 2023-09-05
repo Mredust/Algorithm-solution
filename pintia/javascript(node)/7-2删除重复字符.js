db49f379941b224ca83d2046a890dc9dda9c5317
Mredust <124856279+Mredust@users.noreply.github.com>
2023-09-05 22:30:56 +0800

    Update 7-2删除重复字符.js

--- pintia/javascript(node)/7-2删除重复字符.js
+++ pintia/javascript(node)/7-2删除重复字符.js
@@ -5,12 +5,18 @@ const rl = r.createInterface({
     output: process.stdout
 });
 
+// 提示用户输入
 rl.question('',(input)=>{
+    
+    // 获取字符串中唯一字符
     const uniqueChars = [...new Set(input)];
+    
+    // 对字符进行排序并连接成字符串
     const sortedStr = uniqueChars.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0)).join('');
 
+    // 输出结果
     console.log(`${sortedStr}`);
 
+    // 关闭 readline 接口
     rl.close();
 });
-
